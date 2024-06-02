609508045cefe04584f70dc31997a6c919bcbd89
Helge Klein <helge@helgeklein.com>
2024-06-02 13:31:17 +0200

    Switched to auto-generated Kerberos configuration.

--- init.sh
+++ init.sh
@@ -4,124 +4,125 @@ set -e
 
 appSetup () {
 
-	# Set variables
-	DOMAIN=${DOMAIN:-SAMDOM.LOCAL}
-	DOMAINPASS=${DOMAINPASS:-youshouldsetapassword^123}
-	JOIN=${JOIN:-false}
-	JOINSITE=${JOINSITE:-NONE}
-	NOCOMPLEXITY=${NOCOMPLEXITY:-false}
-	INSECURELDAP=${INSECURELDAP:-false}
-	DNSFORWARDER=${DNSFORWARDER:-NONE}
-	HOSTIP=${HOSTIP:-NONE}
-	RPCPORTS=${RPCPORTS:-"49152-49172"}
-	DOMAIN_DC=${DOMAIN_DC:-${DOMAIN_DC}}
+   # Set variables
+   DOMAIN=${DOMAIN:-SAMDOM.LOCAL}
+   DOMAINPASS=${DOMAINPASS:-youshouldsetapassword^123}
+   JOIN=${JOIN:-false}
+   JOINSITE=${JOINSITE:-NONE}
+   NOCOMPLEXITY=${NOCOMPLEXITY:-false}
+   INSECURELDAP=${INSECURELDAP:-false}
+   DNSFORWARDER=${DNSFORWARDER:-NONE}
+   HOSTIP=${HOSTIP:-NONE}
+   RPCPORTS=${RPCPORTS:-"49152-49172"}
+   DOMAIN_DC=${DOMAIN_DC:-${DOMAIN_DC}}
 
-	LDOMAIN=${DOMAIN,,}
-	UDOMAIN=${DOMAIN^^}
-	URDOMAIN=${UDOMAIN%%.*}
+   LDOMAIN=${DOMAIN,,}
+   UDOMAIN=${DOMAIN^^}
+   URDOMAIN=${UDOMAIN%%.*}
 
-	# Set host ip option
-	if [[ "$HOSTIP" != "NONE" ]]; then
-		HOSTIP_OPTION="--host-ip=$HOSTIP"
-	else
-		HOSTIP_OPTION=""
-	fi
+   # Set host ip option
+   if [[ "$HOSTIP" != "NONE" ]]; then
+      HOSTIP_OPTION="--host-ip=$HOSTIP"
+   else
+      HOSTIP_OPTION=""
+   fi
 
-	# Set up samba
-	mv /etc/krb5.conf /etc/krb5.conf.orig
-	echo "[libdefaults]" > /etc/krb5.conf
-	echo "    dns_lookup_realm = false" >> /etc/krb5.conf
-	echo "    dns_lookup_kdc = true" >> /etc/krb5.conf
-	echo "    default_realm = ${UDOMAIN}" >> /etc/krb5.conf
-	# If the finished file isn't there, this is brand new, we're not just moving to a new container
-	FIRSTRUN=false
-	if [[ ! -f /etc/samba/external/smb.conf ]]; then
-		FIRSTRUN=true
-		mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
-		if [[ ${JOIN,,} == "true" ]]; then
-			if [[ ${JOINSITE} == "NONE" ]]; then
-				samba-tool domain join ${LDOMAIN} DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL
-			else
-				samba-tool domain join ${LDOMAIN} DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL --site=${JOINSITE}
-			fi
-		else
-			samba-tool domain provision --use-rfc2307 --domain=${URDOMAIN} --realm=${UDOMAIN} --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass=${DOMAINPASS} ${HOSTIP_OPTION}
-			if [[ ${NOCOMPLEXITY,,} == "true" ]]; then
-				samba-tool domain passwordsettings set --complexity=off
-				samba-tool domain passwordsettings set --history-length=0
-				samba-tool domain passwordsettings set --min-pwd-age=0
-				samba-tool domain passwordsettings set --max-pwd-age=0
-			fi
-		fi
-		sed -i "/\[global\]/a \
-			\\\tidmap_ldb:use rfc2307 = yes\\n\
-			wins support = yes\\n\
-			template shell = /bin/bash\\n\
-			template homedir = /home/%U\\n\
-			idmap config ${URDOMAIN} : schema_mode = rfc2307\\n\
-			idmap config ${URDOMAIN} : unix_nss_info = yes\\n\
-			idmap config ${URDOMAIN} : backend = ad\\n\
-			rpc server dynamic port range = ${RPCPORTS}\
-			" /etc/samba/smb.conf
-		sed -i "s/LOCALDC/${URDOMAIN}DC/g" /etc/samba/smb.conf
-		if [[ $DNSFORWARDER != "NONE" ]]; then
-			sed -i "/dns forwarder/d" /etc/samba/smb.conf
-			sed -i "/\[global\]/a \
-				\\\tdns forwarder = ${DNSFORWARDER}\
-				" /etc/samba/smb.conf
-		fi
-		if [[ ${INSECURELDAP,,} == "true" ]]; then
-			sed -i "/\[global\]/a \
-				\\\tldap server require strong auth = no\
-				" /etc/samba/smb.conf
-		fi
-		# Once we are set up, we'll make a file so that we know to use it if we ever spin this up again
-		cp -f /etc/samba/smb.conf /etc/samba/external/smb.conf
-	else
-		cp -f /etc/samba/external/smb.conf /etc/samba/smb.conf
-	fi
+   # Set up samba
+   # If the finished file isn't there, this is brand new, we're not just moving to a new container
+   FIRSTRUN=false
+   if [[ ! -f /etc/samba/external/smb.conf ]]; then
+      FIRSTRUN=true
+      mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
+      if [[ ${JOIN,,} == "true" ]]; then
+         if [[ ${JOINSITE} == "NONE" ]]; then
+            samba-tool domain join ${LDOMAIN} DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL
+         else
+            samba-tool domain join ${LDOMAIN} DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL --site=${JOINSITE}
+         fi
+      else
+         # Provision a new AD
+         samba-tool domain provision --use-rfc2307 --domain=${URDOMAIN} --realm=${UDOMAIN} --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass=${DOMAINPASS} ${HOSTIP_OPTION}
 
-	# Set up supervisor
-	echo "[supervisord]" > /etc/supervisor/conf.d/supervisord.conf
-	echo "user=root" >> /etc/supervisor/conf.d/supervisord.conf
-	echo "nodaemon=true" >> /etc/supervisor/conf.d/supervisord.conf
-	echo "" >> /etc/supervisor/conf.d/supervisord.conf
-	echo "[program:ntpd]" >> /etc/supervisor/conf.d/supervisord.conf
-	echo "command=/usr/sbin/ntpd -c /etc/ntpd.conf -n" >> /etc/supervisor/conf.d/supervisord.conf
-	echo "[program:samba]" >> /etc/supervisor/conf.d/supervisord.conf
-	echo "command=/usr/sbin/samba -i" >> /etc/supervisor/conf.d/supervisord.conf
+         # Use the Kerberos configuration the provisioning tool created for us
+         cp -f /var/lib/samba/private/krb5.conf /etc/krb5.conf
 
-	echo "server 127.127.1.0" > /etc/ntpd.conf
-	echo "fudge  127.127.1.0 stratum 10" >> /etc/ntpd.conf
-	echo "server 0.pool.ntp.org     iburst prefer" >> /etc/ntpd.conf
-	echo "server 1.pool.ntp.org     iburst prefer" >> /etc/ntpd.conf
-	echo "server 2.pool.ntp.org     iburst prefer" >> /etc/ntpd.conf
-	echo "driftfile       /var/lib/ntp/ntp.drift" >> /etc/ntpd.conf
-	echo "logfile         /var/log/ntp" >> /etc/ntpd.conf
-	echo "ntpsigndsocket  /usr/local/samba/var/lib/ntp_signd/" >> /etc/ntpd.conf
-	echo "restrict default kod nomodify notrap nopeer mssntp" >> /etc/ntpd.conf
-	echo "restrict 127.0.0.1" >> /etc/ntpd.conf
-	echo "restrict 0.pool.ntp.org   mask 255.255.255.255    nomodify notrap nopeer noquery" >> /etc/ntpd.conf
-	echo "restrict 1.pool.ntp.org   mask 255.255.255.255    nomodify notrap nopeer noquery" >> /etc/ntpd.conf
-	echo "restrict 2.pool.ntp.org   mask 255.255.255.255    nomodify notrap nopeer noquery" >> /etc/ntpd.conf
-	echo "tinker panic 0" >> /etc/ntpd.conf
+         if [[ ${NOCOMPLEXITY,,} == "true" ]]; then
+            samba-tool domain passwordsettings set --complexity=off
+            samba-tool domain passwordsettings set --history-length=0
+            samba-tool domain passwordsettings set --min-pwd-age=0
+            samba-tool domain passwordsettings set --max-pwd-age=0
+         fi
+      fi
+      sed -i "/\[global\]/a \
+         \\\tidmap_ldb:use rfc2307 = yes\\n\
+         wins support = yes\\n\
+         template shell = /bin/bash\\n\
+         template homedir = /home/%U\\n\
+         idmap config ${URDOMAIN} : schema_mode = rfc2307\\n\
+         idmap config ${URDOMAIN} : unix_nss_info = yes\\n\
+         idmap config ${URDOMAIN} : backend = ad\\n\
+         rpc server dynamic port range = ${RPCPORTS}\
+         " /etc/samba/smb.conf
+      sed -i "s/LOCALDC/${URDOMAIN}DC/g" /etc/samba/smb.conf
+      if [[ $DNSFORWARDER != "NONE" ]]; then
+         sed -i "/dns forwarder/d" /etc/samba/smb.conf
+         sed -i "/\[global\]/a \
+            \\\tdns forwarder = ${DNSFORWARDER}\
+            " /etc/samba/smb.conf
+      fi
+      if [[ ${INSECURELDAP,,} == "true" ]]; then
+         sed -i "/\[global\]/a \
+            \\\tldap server require strong auth = no\
+            " /etc/samba/smb.conf
+      fi
+      # Once we are set up, we'll make a file so that we know to use it if we ever spin this up again
+      cp -f /etc/samba/smb.conf /etc/samba/external/smb.conf
+   else
+      cp -f /etc/samba/external/smb.conf /etc/samba/smb.conf
+   fi
 
-	appStart ${FIRSTRUN}
+   # Set up supervisor
+   echo "[supervisord]" > /etc/supervisor/conf.d/supervisord.conf
+   echo "user=root" >> /etc/supervisor/conf.d/supervisord.conf
+   echo "nodaemon=true" >> /etc/supervisor/conf.d/supervisord.conf
+   echo "" >> /etc/supervisor/conf.d/supervisord.conf
+   echo "[program:ntpd]" >> /etc/supervisor/conf.d/supervisord.conf
+   echo "command=/usr/sbin/ntpd -c /etc/ntpd.conf -n" >> /etc/supervisor/conf.d/supervisord.conf
+   echo "[program:samba]" >> /etc/supervisor/conf.d/supervisord.conf
+   echo "command=/usr/sbin/samba -i" >> /etc/supervisor/conf.d/supervisord.conf
+
+   echo "server 127.127.1.0" > /etc/ntpd.conf
+   echo "fudge  127.127.1.0 stratum 10" >> /etc/ntpd.conf
+   echo "server 0.pool.ntp.org     iburst prefer" >> /etc/ntpd.conf
+   echo "server 1.pool.ntp.org     iburst prefer" >> /etc/ntpd.conf
+   echo "server 2.pool.ntp.org     iburst prefer" >> /etc/ntpd.conf
+   echo "driftfile       /var/lib/ntp/ntp.drift" >> /etc/ntpd.conf
+   echo "logfile         /var/log/ntp" >> /etc/ntpd.conf
+   echo "ntpsigndsocket  /usr/local/samba/var/lib/ntp_signd/" >> /etc/ntpd.conf
+   echo "restrict default kod nomodify notrap nopeer mssntp" >> /etc/ntpd.conf
+   echo "restrict 127.0.0.1" >> /etc/ntpd.conf
+   echo "restrict 0.pool.ntp.org   mask 255.255.255.255    nomodify notrap nopeer noquery" >> /etc/ntpd.conf
+   echo "restrict 1.pool.ntp.org   mask 255.255.255.255    nomodify notrap nopeer noquery" >> /etc/ntpd.conf
+   echo "restrict 2.pool.ntp.org   mask 255.255.255.255    nomodify notrap nopeer noquery" >> /etc/ntpd.conf
+   echo "tinker panic 0" >> /etc/ntpd.conf
+
+   appStart ${FIRSTRUN}
 }
 
 fixDomainUsersGroup () {
-	GIDNUMBER=$(ldbedit -H /var/lib/samba/private/sam.ldb -e cat "samaccountname=domain users" | { grep ^gidNumber: || true; })
-	if [ -z "${GIDNUMBER}" ]; then
-		echo "dn: CN=Domain Users,CN=Users,${DOMAIN_DC}
+   GIDNUMBER=$(ldbedit -H /var/lib/samba/private/sam.ldb -e cat "samaccountname=domain users" | { grep ^gidNumber: || true; })
+   if [ -z "${GIDNUMBER}" ]; then
+      echo "dn: CN=Domain Users,CN=Users,${DOMAIN_DC}
 changetype: modify
 add: gidNumber
 gidNumber: 3000000" | ldbmodify -H /var/lib/samba/private/sam.ldb
-		net cache flush
-	fi
+      net cache flush
+   fi
 }
 
 setupSSH () {
-	echo "dn: CN=sshPublicKey,CN=Schema,CN=Configuration,${DOMAIN_DC}
+
+   echo "dn: CN=sshPublicKey,CN=Schema,CN=Configuration,${DOMAIN_DC}
 changetype: add
 objectClass: top
 objectClass: attributeSchema
@@ -136,7 +137,8 @@ isSingleValued: FALSE
 objectCategory: CN=Attribute-Schema,CN=Schema,CN=Configuration,${DOMAIN_DC}
 searchFlags: 8
 schemaIDGUID:: cjDAZyEXzU+/akI0EGDW+g==" > /tmp/Sshpubkey.attr.ldif
-	echo "dn: CN=ldapPublicKey,CN=Schema,CN=Configuration,${DOMAIN_DC}
+
+   echo "dn: CN=ldapPublicKey,CN=Schema,CN=Configuration,${DOMAIN_DC}
 changetype: add
 objectClass: top
 objectClass: classSchema
@@ -151,24 +153,26 @@ objectCategory: CN=Class-Schema,CN=Schema,CN=Configuration,${DOMAIN_DC}
 defaultObjectCategory: CN=ldapPublicKey,CN=Schema,CN=Configuration,${DOMAIN_DC}
 mayContain: sshPublicKey
 schemaIDGUID:: +8nFQ43rpkWTOgbCCcSkqA==" > /tmp/Sshpubkey.class.ldif
-	ldbadd -H /var/lib/samba/private/sam.ldb --option="dsdb:schema update allowed"=true /tmp/Sshpubkey.attr.ldif
-	ldbadd -H /var/lib/samba/private/sam.ldb --option="dsdb:schema update allowed"=true /tmp/Sshpubkey.class.ldif
+
+   ldbadd -H /var/lib/samba/private/sam.ldb --option="dsdb:schema update allowed"=true /tmp/Sshpubkey.attr.ldif
+   ldbadd -H /var/lib/samba/private/sam.ldb --option="dsdb:schema update allowed"=true /tmp/Sshpubkey.class.ldif
 }
 
 appStart () {
-	/usr/bin/supervisord > /var/log/supervisor/supervisor.log 2>&1 &
-	if [ "${1}" = "true" ]; then
-		echo "Sleeping 10 before checking on Domain Users of gid 3000000 and setting up sshPublicKey"
-		sleep 10
-		fixDomainUsersGroup
-		setupSSH
-	fi
-	while [ ! -f /var/log/supervisor/supervisor.log ]; do
-		echo "Waiting for log files..."
-		sleep 1
-	done
-	sleep 3
-	tail -F /var/log/supervisor/*.log
+
+   /usr/bin/supervisord > /var/log/supervisor/supervisor.log 2>&1 &
+   if [ "${1}" = "true" ]; then
+      echo "Sleeping 10 before checking on Domain Users of gid 3000000 and setting up sshPublicKey"
+      sleep 10
+      fixDomainUsersGroup
+      setupSSH
+   fi
+   while [ ! -f /var/log/supervisor/supervisor.log ]; do
+      echo "Waiting for log files..."
+      sleep 1
+   done
+   sleep 3
+   tail -F /var/log/supervisor/*.log
 }
 
 appSetup
